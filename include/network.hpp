234cb866b7d45ff2ceecbcc473f0aeda0ff5928c
Every2 <christiansantoslima21@gmail.com>
2024-06-20 21:11:56 -0300

    feat: implement Network class

---
+++ include/network.hpp
@@ -0,0 +1,17 @@
+#ifndef NETWORK_HPP
+#define NETWORK_HPP
+
+#include <vector>
+
+class Network {
+
+public:
+    Network(std::vector<int>& sizes);
+
+private:
+    std::vector<int> _sizes{};
+    std::size_t numbers_of_layers;
+    std::vector<std::vector<double>> _biases{};
+    std::vector<std::vector<std::vector<double>>> _weights{};
+};
+#endif //NETWORK_HPP
